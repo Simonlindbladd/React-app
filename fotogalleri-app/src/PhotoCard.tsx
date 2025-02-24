d9f5c0b2c9b77e67767d43e23d7c64e97916817a
Simon <simlin1997@gmail.com>
2025-02-24 14:29:36 +0100

    a

--- fotogalleri-app/src/PhotoCard.tsx
+++ fotogalleri-app/src/PhotoCard.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { useMemo } from 'react';
 
 interface Photo {
   id: number;
@@ -12,23 +11,10 @@ interface Props {
 }
 
 const PhotoCard = ({ photo }: Props) => {
-  // Generera en slumpmässig rotation mellan -5 och 5 grader
-  const rotation = useMemo(() => Math.random() * 10 - 5, []);
-
   return (
-    <div
-      className="photo-card"
-      style={{
-        transform: `rotate(${rotation}deg)`,
-        border: '2px solid red', // Inline debug-stil för att se boxen
-      }}
-    >
+    <Link to={`/photo/${photo.id}`} className="photo-card">
       <img src={photo.url} alt={photo.title} />
-      <div style={{ padding: '0.5rem' }}>
-        <h3>{photo.title}</h3>
-        <Link to={`/photo/${photo.id}`}>Visa detaljer</Link>
-      </div>
-    </div>
+    </Link>
   );
 };
 
