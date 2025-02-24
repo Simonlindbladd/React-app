372c11e307d4a554460a06c5e524cf65136c9f39
Simon <simlin1997@gmail.com>
2025-02-24 14:49:31 +0100

    made the image

--- fotogalleri-app/src/PhotoCard.tsx
+++ fotogalleri-app/src/PhotoCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useMemo } from 'react';
 
 interface Photo {
   id: number;
@@ -11,8 +12,16 @@ interface Props {
 }
 
 const PhotoCard = ({ photo }: Props) => {
+  // Randomly choose a column span between 1 and 3.
+  // This will make some images wider than others.
+  const colSpan = useMemo(() => Math.floor(Math.random() * 3) + 1, []);
+
   return (
-    <Link to={`/photo/${photo.id}`} className="photo-card">
+    <Link 
+      to={`/photo/${photo.id}`} 
+      className="photo-card" 
+      style={{ gridColumn: `span ${colSpan}` }}
+    >
       <img src={photo.url} alt={photo.title} />
     </Link>
   );
