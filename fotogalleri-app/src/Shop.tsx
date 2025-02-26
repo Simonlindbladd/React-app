8d788ada177392b9c2fd00061c2d2adba6acfda1
Simon <simlin1997@gmail.com>
2025-02-26 12:57:05 +0100

    added product images

--- fotogalleri-app/src/Shop.tsx
+++ fotogalleri-app/src/Shop.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+// Import a local image for the main product image
+import productMain from './assets/img2.jpg';
+// Import local images for the thumbnails
+import thumb1 from './assets/img3.jpg';
+import thumb2 from './assets/img2.jpg';
+import thumb3 from './assets/img3.jpg';
 import './Shop.css';
 
 const Shop = () => {
@@ -17,19 +23,21 @@ const Shop = () => {
   return (
     <div className="shop-container">
       <div className="shop-images">
+        {/* Main product image using a local asset */}
         <img 
-          src="https://picsum.photos/seed/product/600/600" 
+          src={productMain} 
           alt="Product" 
           className="main-image"
         />
+        {/* Thumbnails using local images */}
         <div className="thumbnails">
-          <img src="https://picsum.photos/seed/product1/200/200" alt="Thumb 1" />
-          <img src="https://picsum.photos/seed/product2/200/200" alt="Thumb 2" />
-          <img src="https://picsum.photos/seed/product3/200/200" alt="Thumb 3" />
+          <img src={thumb1} alt="Thumb 1" />
+          <img src={thumb2} alt="Thumb 2" />
+          <img src={thumb3} alt="Thumb 3" />
         </div>
       </div>
       <div className="shop-info">
-        <h1>Awesome Product</h1>
+        <h1>A Product</h1>
         <p>
           This is a detailed description of the product. It includes features, specifications, and benefits.
         </p>
@@ -37,9 +45,7 @@ const Shop = () => {
         <button onClick={addToCart}>Add to Cart</button>
         {cart > 0 && (
           <div style={{ marginTop: '1rem' }}>
-            <div style={{ fontWeight: 'bold' }}>
-              Items in Basket: {cart}
-            </div>
+            <div style={{ fontWeight: 'bold' }}>Items in Basket: {cart}</div>
             <button onClick={handleCheckout} style={{ marginTop: '1rem' }}>
               Proceed to Checkout
             </button>
